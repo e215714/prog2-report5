7359cff8826b92fbfefe4122914404d50e9e3327
taigao <e215714@ie.u-ryukyu.ac.jp>
2021-12-12 02:20:51 +0900

    step1: preparing the code

---
+++ src/jp/ac/uryukyu/ie/e215714/Exception.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e215714;
+
+public class Exception {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+}
